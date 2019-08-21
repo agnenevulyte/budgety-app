c0f16cade06494a7fc0956ca8eb8b756396253bd
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-08-21 10:08:38 +0100

    budgetController, UIController, controller created

---
+++ app.js
@@ -0,0 +1,56 @@
+// this module controls budget data
+var budgetController = (function() {
+   // some code
+   
+    // var x =23;
+    // var add = function(a) {
+    //     return x + a;
+    // }
+    
+    // // exposed to the public, because the closure was created here
+    // return {
+    //     publicTest: function(b) {
+    //         return add(b);
+    //     }
+    // }
+
+})();
+// -----------------------------------------------------
+
+
+
+
+// UI Controller
+var UIController = (function(){
+
+    // some code
+
+})();
+// -----------------------------------------------------
+
+
+
+
+// connects budgetController & UIController
+var controller = (function(budgetCtrl, UICtrl){
+
+    var ctrlAddItem = function() {
+        // 1. get the field input data
+        // 2. add the item to the budget controller
+        // 3. add the new item to the UI
+        // 4. calculate the budget
+        // 5. display the budget on UI 
+        console.log('dfsf')
+    }
+
+    document.querySelector(".add__btn").addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e) {
+        if (e.keyCode === 13 || e.which === 13) { // 13 is enter
+            ctrlAddItem();
+        }
+    })
+    
+// some code
+
+})(budgetController, UIController);
